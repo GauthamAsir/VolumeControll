5563da71c0cca1ed013acd79604e72570d2199c7
GauthamAsir <gauthamnadar12@gmail.com>
2021-10-24 13:42:38 +0530

    Set Audio Focus To None
    Signed-off-by: GauthamAsir <gauthamnadar12@gmail.com>

--- app/src/main/java/a/gautham/volumecontroll/VolumeTile.java
+++ app/src/main/java/a/gautham/volumecontroll/VolumeTile.java
@@ -2,16 +2,20 @@ package a.gautham.volumecontroll;
 
 import android.content.Context;
 import android.media.AudioManager;
+import android.os.Build;
 import android.service.quicksettings.TileService;
+
+import androidx.annotation.RequiresApi;
 
 public class VolumeTile extends TileService {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onClick() {
         super.onClick();
 
         AudioManager audioManager = (AudioManager) getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
-        audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_SAME, AudioManager.FLAG_SHOW_UI);
+        audioManager.adjustStreamVolume(AudioManager.AUDIOFOCUS_NONE, AudioManager.ADJUST_SAME, AudioManager.FLAG_SHOW_UI);
 
     }
 }
